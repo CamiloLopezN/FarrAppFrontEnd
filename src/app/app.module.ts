3b48c7975131712ed1e9659bb7ffa36f4fa6df11
godthyane <jose.daza01@uptc.edu.co>
2020-12-06 17:33:16 -0500

    Login Component, icons add, header add.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,31 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { FooterComponent } from './footer/footer.component';
-import { RegisterClientComponent } from './client/register-client/register-client.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
+import {FooterComponent} from './footer/footer.component';
+import {RegisterClientComponent} from './client/register-client/register-client.component';
+import {HeaderComponent} from './header/header.component';
+import {LoginComponent} from './login/login.component';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
-    RegisterClientComponent
+    RegisterClientComponent,
+    LoginComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
     FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {faUserCircle, faLock, faExclamationTriangle, faUnlock, faExclamationCircle} from '@fortawesome/free-solid-svg-icons/';
+import {Router} from '@angular/router';
+import {Client} from '../model/client';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,66 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  faUserCircle = faUserCircle;
+  faLock = faLock;
+  faExclamationTriangle = faExclamationTriangle;
+  faUnlock = faUnlock;
+  faExclamation = faExclamationCircle;
+  client: Client;
+  isPass = true;
+  isUserMail = true;
+  emailRetrieve: string;
+  isUserEmailRetrieve = true;
+
+  constructor(
+    private router: Router
+  ) {
+  }
 
   ngOnInit(): void {
   }
 
+  onSubmit(): void {
+    if (!this.isEmailExist()) {
+      this.isUserMail = false;
+      this.isPass = true;
+    } else if (!this.isPassOk()) {
+      this.isUserMail = true;
+      this.isPass = false;
+    } else {
+      this.isUserMail = true;
+      this.isPass = true;
+      this.redirect();
+    }
+  }
+
+  isEmpty(): boolean {
+    return this.client.password === '';
+  }
+
+  isPassOk(): boolean {
+    return this.client.password === 'hola';
+  }
+
+  isEmailExist(): boolean {
+    return this.client.e_mail === 'josedaza@gmail.com';
+  }
+
+  isEmailExistTwo(): boolean {
+    return this.emailRetrieve === 'josedaza@gmail.com';
+  }
+
+  redirect(): void {
+    this.router.navigate(['']);
+
+  }
+
+  retrievePass(): void {
+    this.isUserEmailRetrieve = this.isEmailExistTwo();
+    console.log('Email enviado');
+  }
+
+  resetEmailRetrieve(): void {
+    this.emailRetrieve = '';
+  }
 }

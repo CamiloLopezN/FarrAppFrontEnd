83563dcdb25dc4069e89ab9278426a65c79e7c83
godthyane <jose.daza01@uptc.edu.co>
2020-12-07 11:39:20 -0500

    Remove client in local.

--- src/app/client/profile-client/profile-client.component.ts
+++ src/app/client/profile-client/profile-client.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Client} from '../../../models/client';
+import {ClientServiceService} from '../../services/client-service.service';
+import {ActivatedRoute, Params, Router} from '@angular/router';
+import {faExclamationTriangle} from '@fortawesome/free-solid-svg-icons/faExclamationTriangle';
 
 @Component({
   selector: 'app-profile-client',
@@ -7,9 +11,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileClientComponent implements OnInit {
 
-  constructor() { }
+  public client: Client;
+  public isRemove: boolean;
+  public faExclamationTriangle = faExclamationTriangle;
 
-  ngOnInit(): void {
+  constructor(
+    private _clientService: ClientServiceService,
+    private _route: ActivatedRoute,
+    private _router: Router
+  ) {
   }
 
+  ngOnInit(): void {
+    this.getUser();
+  }
+
+  getUser(): void {
+    this._route.params.forEach((params: Params) => {
+      const id = params.id;
+      this._clientService.getUser(id).subscribe(
+        response => {
+          this.client = response;
+        }, error => {
+          console.log(error as any);
+        }
+      );
+    });
+  }
+
+  removeUser(): void {
+    this._clientService.removeUser(this.client.id);
+    this._router.navigate(['/login']);
+  }
+
+  changeView(): void {
+    this.isRemove = !this.isRemove;
+  }
 }

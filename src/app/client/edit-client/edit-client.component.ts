79aab2284142ad1991c6a9cdbd1f0a96f08dfc36
godthyane <jose.daza01@uptc.edu.co>
2020-12-09 23:26:58 -0500

    Edit client component add

--- src/app/client/edit-client/edit-client.component.ts
+++ src/app/client/edit-client/edit-client.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ClientResponse} from '../../model/client';
+import {ActivatedRoute, Router} from '@angular/router';
+import {ClientService} from '../../services/client.service';
+import {DatePipe} from '@angular/common';
 
 @Component({
   selector: 'app-edit-client',
@@ -7,9 +11,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditClientComponent implements OnInit {
 
-  constructor() { }
+  public client: ClientResponse;
+  public isRemove: boolean;
 
-  ngOnInit(): void {
+  constructor(
+    private _route: ActivatedRoute,
+    private _router: Router,
+    private clientS: ClientService,
+    private datePipe: DatePipe
+  ) {
+    this.client = {
+      id: '',
+      name: '',
+      lastname: '',
+      birthdate: '',
+      gender: ''
+    };
   }
 
+  ngOnInit(): void {
+    this.getUser();
+  }
+
+  getUser(): void {
+    this.clientS.getUser().subscribe((res) => {
+      this.client = {
+        id: res.search._id,
+        name: res.search.name,
+        lastname: res.search.lastname,
+        birthdate: this.datePipe.transform(res.search.birthdate, 'dd-MM-YYYY'),
+        gender: res.search.gender
+      };
+
+    });
+  }
+
+  changeView(): void {
+    this.isRemove = !this.isRemove;
+  }
+
+  return(): void {
+    this._router.navigate(['/client/' + this.client.id]);
+  }
+
+  edit(): void {
+
+  }
 }

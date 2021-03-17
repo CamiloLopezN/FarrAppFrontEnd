6a263b0c451c2676fcf3148979c95923117a3ff2
godthyane <jose.daza01@uptc.edu.co>
2021-03-16 22:49:52 -0500

    Se agregó un nuevo módulo de login, se optó por un diseño modal.

---
+++ src/app/login-modal/login-modal.component.ts
@@ -0,0 +1,137 @@
+import {ChangeDetectorRef, Component, OnInit, ViewChild} from '@angular/core';
+import {faExclamationCircle, faUserCircle, faLock, faExclamationTriangle, faUnlock, faChevronLeft} from '@fortawesome/free-solid-svg-icons';
+import {ClientLogin} from '../model/client';
+import {Router} from '@angular/router';
+import {AuthService} from '../services/auth.service';
+import {NotificationService} from '../services/notification.service';
+import {NgForm} from '@angular/forms';
+
+
+declare var $: any;
+
+@Component({
+  selector: 'app-login-modal',
+  templateUrl: './login-modal.component.html',
+  styleUrls: ['./login-modal.component.css']
+})
+export class LoginModalComponent implements OnInit {
+
+  faUserCircle = faUserCircle;
+  faLock = faLock;
+  faExclamationTriangle = faExclamationTriangle;
+  faUnlock = faUnlock;
+  faExclamation = faExclamationCircle;
+  faChevronLeft = faChevronLeft;
+  client: ClientLogin;
+  // tslint:disable-next-line:variable-name
+  e_mail = '';
+  password = '';
+  isPass = true;
+  isUserMail = true;
+  emailRetrieve: string;
+  error: string;
+  isError: boolean;
+  rol;
+
+  isLogin: boolean;
+  errorMessage: string;
+
+
+  @ViewChild('formUser') formRecipe: NgForm;
+  constructor(
+    private router: Router,
+    public authService: AuthService,
+    private notifyS: NotificationService,
+    private changeDetectorRef: ChangeDetectorRef
+  ) {
+  }
+
+  ngOnInit(): void {
+    this.authService.isLog.subscribe(login => {
+      this.isLogin = login;
+    });
+
+    $(document).ready(() => {
+      $('#login-modal').on('show.bs.modal', () => {
+        this.emailRetrieve = '';
+        this.password = '';
+        this.e_mail = '';
+      });
+      $('#login-modal').on('hidden.bs.modal', () => {
+        this.formRecipe.reset();
+        this.changeDetectorRef.detectChanges();
+      });
+    });
+  }
+
+  onSubmit(): void {
+    this.client = {
+      e_mail: this.e_mail,
+      password: this.password
+    };
+    this.errorMessage = '';
+    this.authService.login(this.client).subscribe(() => {
+        this.isPass = true;
+        this.isUserMail = true;
+        $('#login-modal').modal('hide');
+        this.formRecipe.reset();
+        this.changeDetectorRef.detectChanges();
+        this.notifyS.sucessLogin();
+        this.authService.roled.subscribe(rol =>
+          this.rol = rol
+        );
+        if (this.rol == 'superAdmin') {
+          this.redirectAdmin();
+        } else {
+          this.redirect();
+        }
+        this.formRecipe.reset();
+        this.changeDetectorRef.detectChanges();
+      },
+      error => {
+        this.errorMessage = error.error.message;
+        if (this.errorMessage === 'Error la contraseña es incorrecta') {
+          this.isUserMail = true;
+          this.isPass = false;
+        } else {
+          this.isUserMail = false;
+          this.isPass = true;
+        }
+      }
+    );
+  }
+
+  isUndefined(): boolean {
+    return this.errorMessage === undefined;
+  }
+
+  isEmpty(): boolean {
+    return this.password === '';
+  }
+
+  isPassOk(): boolean {
+    return this.password === 'hola';
+  }
+
+  redirect(): void {
+    this.router.navigate(['']);
+
+  }
+
+  redirectAdmin(): void {
+    this.router.navigate(['admin/dashboard']);
+
+  }
+
+  retrievePass(): void {
+    this.authService.retrievePass(this.emailRetrieve).subscribe(res => {
+      console.log(res);
+    }, error => {
+      this.error = error.error.message;
+    });
+  }
+
+  resetEmailRetrieve(): void {
+    this.emailRetrieve = '';
+  }
+}

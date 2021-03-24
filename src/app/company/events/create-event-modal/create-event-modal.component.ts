bb37968c2615f1221e775ecb9782f5bd54e5de5d
godthyane <jose.daza01@uptc.edu.co>
2021-03-24 16:37:40 -0500

    :sparkles: Se hicieron algunos cambios

--- src/app/company/events/create-event-modal/create-event-modal.component.ts
+++ src/app/company/events/create-event-modal/create-event-modal.component.ts
@@ -7,6 +7,12 @@ import {Component, OnInit} from '@angular/core';
 })
 export class CreateEventModalComponent implements OnInit {
 
+  nameEvent: string;
+  dateInit: string;
+  dateInitHour: string;
+  dateFin: string;
+  dateFinHour: string;
+
   constructor() {
   }
 

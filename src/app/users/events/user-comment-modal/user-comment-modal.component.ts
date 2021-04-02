984daa711f8bcb0784d12c71e401523b05175b39
godthyane <jose.daza01@uptc.edu.co>
2021-04-02 16:30:25 -0500

    :sparkles: Se crea la maquetación de comentar un evento como cliente

--- src/app/users/events/user-comment-modal/user-comment-modal.component.ts
+++ src/app/users/events/user-comment-modal/user-comment-modal.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {faStar as fs} from '@fortawesome/free-solid-svg-icons';
+import {faStar} from '@fortawesome/free-regular-svg-icons';
 
 @Component({
   selector: 'app-user-comment-modal',
@@ -7,9 +9,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserCommentModalComponent implements OnInit {
 
-  constructor() { }
+  stars: boolean[];
+  faStartSolid = fs;
+  faStart = faStar;
+  qualification = 0;
+
+  constructor() {
+    this.stars = [false, false, false, false, false];
+  }
 
   ngOnInit(): void {
   }
 
+  refresh(): void {
+    this.stars.forEach((v, i, a) => a[i] = false);
+  }
+
+  setStarsClick(i: number): void {
+    this.refresh();
+    this.qualification = i + 1;
+    for (let j = 0; j <= i; j++) {
+      this.stars[j] = true;
+    }
+  }
 }

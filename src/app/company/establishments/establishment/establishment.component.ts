ea44d601b8558bdf0ec778806c4460aed0b5e971
godthyane <jose.daza01@uptc.edu.co>
2021-05-03 10:53:13 -0500

    :sparkles: Borrar y editar establecimiento

--- src/app/company/establishments/establishment/establishment.component.ts
+++ src/app/company/establishments/establishment/establishment.component.ts
@@ -1,6 +1,9 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {EstablishmentView} from '../../../model/company';
 import {Router} from '@angular/router';
+import {faEdit, faTrash } from '@fortawesome/free-solid-svg-icons';
+import {EventEmmiterService} from '../../../services/event-remove.service';
+import {IsShowModalService} from '../../../services/is-show-modal.service';
 
 declare var $: any;
 
@@ -13,7 +16,10 @@ export class EstablishmentComponent implements OnInit {
 
   @Input() establishment: EstablishmentView;
 
-  constructor(private router: Router) {
+  faTrash = faTrash;
+  faEdit = faEdit;
+
+  constructor(public router: Router, private ers: EventEmmiterService, private ism: IsShowModalService) {
   }
 
   ngOnInit(): void {
@@ -23,6 +29,36 @@ export class EstablishmentComponent implements OnInit {
   }
 
   redirect(): void {
-    this.router.navigate(['/company/establishments/', this.establishment._id]);
+    this.router.navigate(['company', this.establishment.companyId, 'establishments', this.establishment.establishmentId]);
+  }
+
+  edit(): void {
+    try {
+      this.ers.establishmentSelect.next({
+        idCompany: this.establishment.companyId,
+        idEstablishment: this.establishment.establishmentId,
+        name: this.establishment.establishmentName
+      });
+      this.ism.isEstablishment.next(true);
+      this.ism.isEstablishmentEdit.next(true);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      $('#register-event-modal').modal('show');
+    }
+  }
+
+  remove(): void {
+    try {
+      this.ers.establishmentSelect.next({
+        idCompany: this.establishment.companyId,
+        idEstablishment: this.establishment.establishmentId,
+        name: this.establishment.establishmentName
+      });
+    } catch (error) {
+      console.error(error);
+    } finally {
+      $('#removeEvent').modal('show');
+    }
   }
 }
